293bf887d034640eb2b07669eb40278a2046b9c6
rk <rk@emperor-brains>
2018-11-01 00:19:14 +0530

    Finally Whole Application completed.....

--- fb-config.php
+++ fb-config.php
@@ -1,16 +1,17 @@
 <?php 
 
-    //define('APP_ID', '281304975925816');
-    //define('APP_SECRET', 'c9c799d0727f20bcd31187f166ac0c68');
-
     define('APP_ID', '730774217279573');
     define('APP_SECRET', '087985c1445c3e2cc1320d39fcc01029');
     
+    /* for localhost */ 
     define('REDIRECT_URL', 'http://localhost:8080/rtCamp/');
-    
-
     define('DOMAIN',"http://localhost:8080/rtCamp/");
-
+    /* end*/
     
-	require_once './lib/Facebook/autoload.php';
+    /* for live application*/ 
+    define('REDIRECT_URL', 'https://zerothcode.com/rajukhunt/');
+    define('DOMAIN',"https://zerothcode.com/rajukhunt/");
+    /* end */
+
+    require_once './lib/Facebook/autoload.php';
 ?>
