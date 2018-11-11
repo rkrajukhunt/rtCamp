b5da4cf72d4258bdd071153e224964cb1a12cad4
rk <rk@emperor-brains>
2018-11-11 19:37:28 +0530

    fix bug and slider approve.....

--- my-albums.php
+++ my-albums.php
@@ -176,8 +176,10 @@
         <div class="modal-content">
           <!-- Modal Header -->
           <div class="modal-header">
+            <h4 class="modal-title">My Album</h4>
             <button type="button" class="close" data-dismiss="modal">&times;</button>
           </div>
+          
           <!-- Modal body -->
           <div class="modal-body">
             <div id="demo" class="carousel slide" data-ride="carousel">
@@ -191,7 +193,13 @@
                   <span class="carousel-control-next-icon"></span>
                 </a>
             </div>
-        </div> 
+        </div>
+        
+          <!-- Modal footer -->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+          </div>
+          
     </div>
 </div>
 </body>
